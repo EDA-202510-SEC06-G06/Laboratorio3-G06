842c6822b618f0799f93339a0b22047abbf8f199
mjcaro <majocaro4024@gmail.com>
2025-02-05 15:27:39 -0500

    Cambios Maria

---
+++ DataStructures/List/single_linked_list.py
@@ -0,0 +1,55 @@
+def new_list():
+    newlist = {
+        "first": None,
+        "last": None,
+        "size": 0,
+    }
+    return newlist
+
+def get_element(my_list, pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos:
+        node = node["next"]
+        searchpos += 1
+    return node["info"]
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp = my_list["first"]
+    count = 0
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) == 0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count += 1
+            
+    if not is_in_array:
+        count = -1
+    return count
+
+def add_first(my_list, element):
+    if is_empty(my_list):
+        node= {"info": element, "next": None}
+        my_list["first"] = node
+        my_list["size"] += 1
+        my_list["last"] = node
+    else:
+        node= {"info": element, "next": my_list["first"]}
+        my_list["first"] = node
+        my_list["size"] += 1
+    return my_list
+
+def add_last(my_list, element):
+    if is_empty(my_list):
+        node= {"info": element, "next": None}
+        my_list["first"] = node
+        my_list["size"] += 1
+        my_list["last"] = node
+    else:
+        node= {"info": element, "next": None}
+        my_list["size"] += 1
+        my_list["last"] = node
+    return my_list
+
