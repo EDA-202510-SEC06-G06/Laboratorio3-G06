aa6c6c39b517be227831c32642cc3389364dc548
DonPedro15 <pedropablomunozprieto@gmail.com>
2025-02-09 20:23:33 -0500

    Fix para single linked list

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -7,7 +7,7 @@ def new_list():
     return newlist
 
 def is_empty(my_list):
-    if len(my_list) == 0:
+    if my_list["size"] == 0:
         return True
     else:
         return False
@@ -24,6 +24,7 @@ def is_present(my_list, element, cmp_function):
     is_in_array = False
     temp = my_list["first"]
     count = 0
+    
     while not is_in_array and temp is not None:
         if cmp_function(element, temp["info"]) == 0:
             is_in_array = True
@@ -36,15 +37,18 @@ def is_present(my_list, element, cmp_function):
     return count
 
 def add_first(my_list, element):
+    
     if is_empty(my_list):
         node= {"info": element, "next": None}
         my_list["first"] = node
         my_list["size"] += 1
         my_list["last"] = node
+        
     else:
         node= {"info": element, "next": my_list["first"]}
         my_list["first"] = node
         my_list["size"] += 1
+        
     return my_list
 
 def add_last(my_list, element):
